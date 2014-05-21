93c9abb4e6503197db2448f5a0ec35ce7986d6b0
Issei KOMATSU <issei@ikmz.net>
2014-05-21 09:54:45 +0900

    init

---
+++ meigen.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# meigen.sh # 実行方法
+# $ meigen.sh meigen.list
+#
+# meigen.list : 名言リスト
+
+# Slack 設定
+source ./config.sh
+
+# 名言ファイルの指定
+#   名言ファイルのフォーマット（タブ区切り）
+#   1列: 名言本文
+#   2列: 著者・発言者
+meigenfile=./meigen.list
+
+# 名言ファイルからランダムに一行選択し、フォーマット変更
+text=$(head -$(($RANDOM % $(wc -l ${meigenfile} | cut -d' ' -f 1))) ${meigenfile} | tail -1 | awk -F'	' '{print "\""$1"\"" " by " $2}')
+
+# Slack に通知 
+curl -X POST 'https://slack.com/api/chat.postMessage' -d token=${slack_token} -d text="${text}" -d channel=${slack_notification_channel} -d username=${slack_username} > /dev/null 2>&1
